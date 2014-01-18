1dd021d79f9a02833fe2e966992e9844b76b4811
donwon <imdonw@gmail.com>
2014-01-17 17:44:02 -0800

    Create Edit and Destroy

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,33 @@
 class ProjectsController < ApplicationController
+
+def index
+	@projects = Project.all
+end
+
+def new
+	@project = Project.new
+end
+
+def create
+	project = Project.new params.require(:project).permit([:project_no, :client_name, :title, :description, :status])
+	project.save
+	redirect_to projects_path
+end
+
+def show
+	@project = Project.find(params[:id])
+
+end
+
+def edit
+	@project = Project.find(params[:id])
+end
+
+def destroy
+	@project = Project.find(params[:id])
+	@project.destroy
+	redirect_to projects_path
+end
+
+
 end
