7667b588dd8621e82678f560555690ab7dab35c4
donwon <imdonw@gmail.com>
2014-01-20 12:22:09 -0800

    Fix Like Button Bug

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -43,25 +43,25 @@ end
 
 def like
 	session[:project_ids] ||= []
-	session[:project_ids] << params[:id]
-	@liked_ids = session[:project_ids].split('')
-	Rails.logger.info @liked_ids
-	Rails.logger.info '>>>>>>>>>>>>>>>>>>>>>>'
-	Rails.logger.info @likes.kind_of?(Array)
-	Rails.logger.info '@likes is array? >>>>>>>>>>>>>>>>>>>>>'
-
 	@discussion = Discussion.find(params[:id])
+	@discussion.counter ||=0
+
+unless session[:project_ids].include? params[:id]
+  @discussion.counter += 1 
+  session[:project_ids] << params[:id]
+  @discussion.save
+end
+
 
 	#make new hash to count the project_ids stored in session array
-	@like_count = Hash.new(0)
-	@liked_ids.each { |v| @like_count[v] += 1 }
+	# @like_count = Hash.new(0)
+	# @liked_ids.each { |v| @like_count[v] += 1 }
 	
-	@number_of_likes = @like_count[params[:id]]
-	Rails.logger.info @number_of_likes
-	Rails.logger.info '>>>above is number of likes for this id>>>>>>>>>>>>>>>>>>>'
+	# @number_of_likes = @like_count[params[:id]]
+	#Rails.logger.info @number_of_likes
+	#Rails.logger.info '>>>above is number of likes for this id>>>>>>>>>>>>>>>>>>>'
 
-	@discussion.counter = @number_of_likes
-	@discussion.save
+
 
  redirect_to discussions_path
 end
